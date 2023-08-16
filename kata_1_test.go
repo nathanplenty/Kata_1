27d6fd56354d07d461d42776e314bb4d058aaa44
nathanplenty <nathan.roskamp@plentysystems.com>
2023-08-16 09:57:24 +0200

    Update kata_1_test.go
    Finished Prototype

--- kata_1_test.go
+++ kata_1_test.go
@@ -14,3 +14,32 @@ func TestIsEmpty(t *testing.T) {
 	}
 	fmt.Println(stack)
 }
+
+func TestPushPeek(t *testing.T) {
+	stack := NewStack()
+	stack.Push(5)
+	i, b := stack.Peek()
+	v := stack.Value[i]
+	if v != 5 {
+		t.Errorf("v = %v; want 5", v)
+	}
+	if b != true {
+		t.Errorf("b = %v; want true", b)
+	}
+	fmt.Println(stack)
+}
+
+func TestPop(t *testing.T) {
+	stack := NewStack()
+	stack.Push(5)
+	stack.Push(10)
+	i, b := stack.Pop()
+	v := stack.Value[i-1]
+	if v != 5 {
+		t.Errorf("v = %v; want 5", v)
+	}
+	if b != true {
+		t.Errorf("b = %v; want true", b)
+	}
+	fmt.Println(stack)
+}
